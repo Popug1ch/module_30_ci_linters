7f2d284ff32e665a4d24676fba016905cee69c9e
Max <popugai4iks.maximka@mail.ru>
2025-12-18 20:28:39 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite+aiosqlite:///./database.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True,)
+
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession,
+)
+
+Base = declarative_base()
+
+
+async def get_db() -> AsyncSession:
+    async with async_session() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+
+
+async def create_tables() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
