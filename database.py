5e31815d90f8dd27fcbd2bf5bcd85d438bdfa15c
Max <popugai4iks.maximka@mail.ru>
2025-12-18 21:06:38 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./database.db"
 
-engine = create_async_engine(DATABASE_URL, echo=True,)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+)
 
 async_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession,
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
 )
 
 Base = declarative_base()
