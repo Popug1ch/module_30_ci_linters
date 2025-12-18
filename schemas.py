c6a5623a7ab87200c991d9d1ff285c17c6658957
Max <popugai4iks.maximka@mail.ru>
2025-12-18 20:29:52 +0300

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+
+
+class BaseRecipe(BaseModel):
+    name: str
+    cooking_time: int
+    ingredients: str
+    description: str
+
+
+class RecipeIn(BaseRecipe):
+    """
+    Входящая схема для создания рецепта (POST /recipes).
+    """
+
+
+class RecipeOut(BaseRecipe):
+    """
+    Схема для детального рецепта и создания.
+    """
+    id: int
+    views: int
+
+    class Config:
+        orm_mode = True
+
+
+class RecipeListItem(BaseModel):
+    """
+    Схема для списка рецептов на главном экране.
+    """
+    id: int
+    name: str
+    cooking_time: int
+    views: int
+
+    class Config:
+        orm_mode = True
