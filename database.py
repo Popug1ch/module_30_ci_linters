c1e1e6ba8d8bb88bfb08a36bfd21c5af5bf6e42e
Max <popugai4iks.maximka@mail.ru>
2025-12-19 16:34:00 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import os
 
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
@@ -11,7 +12,7 @@ engine = create_async_engine(
     echo=False,
 )
 
-async_session = sessionmaker(
+async_session = async_sessionmaker(
     engine,
     expire_on_commit=False,
     class_=AsyncSession,
@@ -20,7 +21,7 @@ async_session = sessionmaker(
 Base = declarative_base()
 
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]::
     async with async_session() as session:
         try:
             yield session
