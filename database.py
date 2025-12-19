d15ee400975aac43a84996b4ba1b6ac3de904974
Max <popugai4iks.maximka@mail.ru>
2025-12-19 16:52:48 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 import os
 from collections.abc import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (
-    AsyncSession, async_sessionmaker, create_async_engine)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./database.db")
